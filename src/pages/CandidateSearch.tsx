d3181606da267b7ea4125e30c90d4937cf7a8314
Jacob Garland <352cjg@gmail.com>
2025-01-02 00:05:28 -0500

    started building the search function

--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,7 +1,46 @@
 import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
+import { Candidate } from '../interfaces/Candidate.interface';
+import ErrorPage from './ErrorPage';
 
 const CandidateSearch = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<boolean>(false);
+  const [currentCandidate, setCurrentCandidate] = useState<Candidate>({
+    name: '',
+    login: '',
+    avatar_url: '',
+    email: '',
+    location: '',
+    company: '',
+    bio: ''
+  });
+  const [SavedCandidates, setSavedCandidates] = useState<any[]>([]);
+
+  useEffect(() => {
+    fetchCandidate()
+  })
+
+  const fetchCandidate = async () => {
+    setLoading(true);
+    setError(false);
+    try {
+      const users = await searchGithub();
+      if (users.length > 0) {
+        const randomUser = users[Math.floor(Math.random() * users.length)];
+        const candidateInfo = await searchGithubUser(randomUser.login);
+        setCurrentCandidate(candidateInfo);
+      } else {
+        console.log('No users found');
+        setError(true);
+      }
+    } catch (error) {
+      console.log('Error fetching candidate:', error);
+      setError(true);
+    } finally {
+      setLoading(false);
+    };
+
   return <h1>CandidateSearch</h1>;
 };
 
