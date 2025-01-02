db013b9d11016252276184126fe1259245e5b3a4
Jacob Garland <352cjg@gmail.com>
2025-01-01 22:41:16 -0500

    added navbar and canidate interface, github token

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,16 @@
-const Nav = () => {
-  // TODO: Add necessary code to display the navigation bar and link between the pages
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+// D.C. DONE? TODO: Add necessary code to display the navigation bar and link between the pages
+const Nav: React.FC = () => {
   return (
-    <div>Nav</div>
-  )
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/src/pages/SavedCandidates.tsx">Potential Canidates</Link></li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Nav;
--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,10 @@
-// TODO: Create an interface for the Candidate objects returned by the API
+// DONE TODO: Create an interface for the Candidate objects returned by the API
+export interface Candidate {
+    name: string;
+    login: string;
+    avatar_url: string;
+    email: string;
+    location: string;
+    company: string;
+    bio: string;
+}
