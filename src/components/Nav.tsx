77fb05d3037b59b2f1ba7851eeee51a6188670d9
Jacob Garland <352cjg@gmail.com>
2025-01-11 17:31:45 -0500

    styled tables, fixed nav links

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,29 +1,14 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 
-interface NavLinkProps {
-  to: string;
-  label: string;
-}
-
-// TODO: Add necessary code to display the navigation bar and link between the pages
-const Nav: React.FC = () => {
-  const navLinks: NavLinkProps[] = [
-    { to: '/src/pages/CandidateSearch.tsx', label: 'Home' },
-    { to: '/src/pages/SavedCandidates.tsx', label: 'Potential Candidates' }
-  ];
-
+const Nav = () => {
   return (
-    <nav className="nav">
-      <ul className='nav-item'>
-        {navLinks.map((link, index) => (
-          <li key={index}>
-            <Link to={link.to}>{link.label}</Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
-  );
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/SavedCandidates">Saved Candidates</Link>
+      </nav>
+    </div>
+  )
 };
 
 export default Nav;
