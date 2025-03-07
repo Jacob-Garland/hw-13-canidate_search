cd6c909d20e31f84bf6efebb649b8cc548a232fc
Jacob Garland <352cjg@gmail.com>
2025-03-06 20:44:19 -0500

    trying to get render to deploy, lost on port

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  envDir: './env',
+  envDir: './enviroment',
   plugins: [react()],
+  server: {
+    port: 5000,
+  },
 });
